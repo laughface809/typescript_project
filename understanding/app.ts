2e6268f9c4377f42d46cf5182c5672c8df64765c
bsena-it <bsena-it@aidhub.org>
2022-07-05 23:25:21 +0800

    type in javascript

--- understanding/app.ts
+++ understanding/app.ts
@@ -1,12 +1,19 @@
-function add(n1: number, n2: number){
+function add(n1: number, n2: number, result: boolean, phrase: string){
     // if(typeof n1 !== "number" || typeof n2 !== "number"){
     //     throw new Error('Incorrect input!');
     // }
-    return n1 + n2;
+    const numberProcessing = n1 + n2
+    if(result){
+        console.log(phrase + numberProcessing)
+    }
+    else{
+        return numberProcessing;
+    }
 }
 
 const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result is: ';
 
-const result = add(number1, number2);
-console.log(result)
+const result = add(number1, number2, printResult, resultPhrase);
