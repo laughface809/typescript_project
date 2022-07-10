e136b515b3660d9a502953f4af892b12439ed319
bsena-it <bsena-it@aidhub.org>
2022-07-10 23:17:18 +0800

    never types

--- understanding/app.js
+++ understanding/app.js
@@ -5,3 +5,7 @@ userInput = 'Lauhgface';
 if (typeof userInput === 'string') {
     userName = userInput;
 }
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError('An Error occured!', 500);
--- understanding/app.ts
+++ understanding/app.ts
@@ -6,3 +6,9 @@ userInput = 'Lauhgface';
 if(typeof userInput === 'string'){
     userName = userInput
 }
+
+function generateError(message: string, code: number): never {
+    throw {message: message, errorCode: code};
+}
+
+generateError('An Error occured!', 500)
