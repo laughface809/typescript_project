b69ca6f22db48d06520cdc31c6ae6852cefbe2ae
bsena-it <bsena-it@aidhub.org>
2022-07-10 22:50:40 +0800

    function as types

--- understanding/app.js
+++ understanding/app.js
@@ -1,16 +1,6 @@
-function combine(input1, input2, resultConversion) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === "number" || resultConversion === "as-number") {
-        result = +input1 + +input2; //harus ditambah '+' didepan variable karna kondisinya ada ||
-    }
-    else {
-        result = input1.toLocaleString() + input2.toLocaleString();
-    }
-    return result;
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
-var combineStringAges = combine('30', '26', 'as-number');
-console.log(combineStringAges);
-var combinedNames = combine('laughface', '809', 'as-text');
-console.log(combinedNames);
+var combineValues;
+combineValues = add;
+console.log(combineValues(8, 8));
--- understanding/app.ts
+++ understanding/app.ts
@@ -1,56 +1,9 @@
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text';
-
-function combine(
-    input1: Combinable, 
-    input2: Combinable,
-    resultConversion: ConversionDescriptor,
-){
-    let result;
-    if(typeof input1 === 'number' && typeof input2 === "number" || resultConversion === "as-number"){
-        result = +input1 + +input2; //harus ditambah '+' didepan variable karna kondisinya ada ||
-    }
-    else{
-        result = input1.toLocaleString() + input2.toLocaleString();
-    }
-    return result;
+function add(n1: number, n2: number){
+    return n1 + n2
 }
 
-const combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
+let combineValues: (a: number, b: number) => number;
 
-const combineStringAges = combine('30', '26', 'as-number');
-console.log(combineStringAges)
+combineValues = add;
 
-const combinedNames = combine('laughface', '809', 'as-text');
-console.log(combinedNames)
-
-//Type Aliases & Object Types
-// Type aliases can be used to "create" your own types. You're not limited to storing union types though - you can also provide an alias to a (possibly complex) object type.
-
-// For example:
-
-// type User = { name: string; age: number };
-// const u1: User = { name: 'Max', age: 30 }; // this works!
-// This allows you to avoid unnecessary repetition and manage types centrally.
-
-// For example, you can simplify this code:
-
-// function greet(user: { name: string; age: number }) {
-//   console.log('Hi, I am ' + user.name);
-// }
- 
-// function isOlder(user: { name: string; age: number }, checkAge: number) {
-//   return checkAge > user.age;
-// }
-// To:
-
-// type User = { name: string; age: number };
- 
-// function greet(user: User) {
-//   console.log('Hi, I am ' + user.name);
-// }
- 
-// function isOlder(user: User, checkAge: number) {
-//   return checkAge > user.age;
-// }
+console.log(combineValues(8, 8))
