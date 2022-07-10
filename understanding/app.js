d02b515cf32041dfce0c3d44ef7cb8c8aa2e8928
bsena-it <bsena-it@aidhub.org>
2022-07-10 22:59:44 +0800

    function types and callbacks

--- understanding/app.js
+++ understanding/app.js
@@ -1,6 +1,8 @@
-function add(n1, n2) {
-    return n1 + n2;
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
 }
-var combineValues;
-combineValues = add;
-console.log(combineValues(8, 8));
+addAndHandle(8, 8, function (result) {
+    console.log(result);
+});
+//trying using callback function
--- understanding/app.ts
+++ understanding/app.ts
@@ -1,9 +1,10 @@
-function add(n1: number, n2: number){
-    return n1 + n2
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void){
+    const result = n1 + n2
+    cb(result)
 }
 
-let combineValues: (a: number, b: number) => number;
+addAndHandle(8, 8, (result) => {
+    console.log(result)
+})
 
-combineValues = add;
-
-console.log(combineValues(8, 8))
+//trying using callback function
