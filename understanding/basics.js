8ccfe3950ad29c23ccbefe21a791063b877fc882
bsena-it <bsena-it@aidhub.org>
2022-07-15 23:14:44 +0800

    using tsc init and watch mode

--- understanding/basics.js
+++ understanding/basics.js
@@ -1,3 +1,4 @@
+"use strict";
 // function add(n1: number, n2: number, showResult: boolean, phrase: string) {
 //   // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
 //   //   throw new Error('Incorrect input!');
@@ -9,11 +10,9 @@
 //     return result;
 //   }
 // }
-
 // let number1: number;
 // number1 = 5;
 // const number2 = 2.8;
 // const printResult = true;
 // let resultPhrase = 'Result is: ';
-
 // add(number1, number2, printResult, resultPhrase);
